56163acd786e9c34e6718ff85782964c5f0839ab
admiralnelson <admiralofinternetmeme@outlook.com>
2024-12-26 09:18:22 +0700

    repanse AI stuff

--- campaign/mod/RepanseRoyaleArmoury.ts
+++ campaign/mod/RepanseRoyaleArmoury.ts
@@ -1,7 +1,47 @@
 namespace RepanseRoyaleArmoury {
+    const api = BretonniaInGameKitbash
+
+    function IsRepanseAIControlled() : boolean {
+        const repanseFaction = api.GetFactionByKey("wh2_dlc14_brt_chevaliers_de_lyonesse")
+        const isAiControlled = repanseFaction?.IsHuman
+
+        return !isAiControlled
+    }
+
+    function GiveRepanseHerSpecialItemsForAI() {
+        if(!IsRepanseAIControlled()) return
+
+        const repanseFaction = api.GetFactionByKey("wh2_dlc14_brt_chevaliers_de_lyonesse")
+        const factionLeader = repanseFaction?.FactionLeader
+        if(factionLeader == null) return
+
+        const kitbashedRepanse = api.KitbashedCharacter.TryCast(factionLeader)
+        if(kitbashedRepanse == null) return
+
+        const availableArmouryItems = kitbashedRepanse.AvailableArmoryItems
+
+        const hasTheTorsoArmour = availableArmouryItems.includes("kitbasher_repanse_blessed_armor_of_the_lady")
+        if(hasTheTorsoArmour) return
+
+        //step 1. give her torso armour
+        setTimeout(() => {
+            factionLeader.AddArmoryItem("kitbasher_repanse_blessed_armor_of_the_lady", true, true)
+        }, 100)
+
+        //step 2. give her leg armour
+        setTimeout(() => {
+            factionLeader.AddArmoryItem("kitbasher_repanse_greaves_of_the_lady", true, true)
+        }, 150)
+
+        //step 3. give her pauldron armour
+        setTimeout(() => {
+            factionLeader.AddArmoryItem("kitbasher_repanse_pauldrons_of_the_lady", true, true)
+        }, 200)
+    }
 
     function Init() {
-        const api = BretonniaInGameKitbash
+
+        GiveRepanseHerSpecialItemsForAI()
 
         core.add_listener(
             "when repanse received sword of lyonesse",
@@ -11,6 +51,8 @@ namespace RepanseRoyaleArmoury {
                 if(context.character == null) return false
                 const character = context.character()
                 const ancillary = context.ancillary()
+                
+                if(IsRepanseAIControlled()) return false
 
                 const isSwordOfLyonese = ancillary === "wh2_dlc14_anc_weapon_sword_of_lyonesse"
                 const lord  = api.WrapICharacterObjectToCharacter(character)
